006bfedf35649c42402fd2a4f0c0508338f6a202
chrisashwalker <hello@chrisashwalker.com>
2020-07-01 20:47:29 +0100

    Update

--- run.py
+++ run.py
@@ -1,16 +1,54 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 templates = ('q1.html', 'q2.html', 'q3.html', 'q4.html', 'q5.html', 'wd.html')
 
 
 @app.route("/", methods=['POST', 'GET'])
-def run():
+@app.route("/q1", methods=['POST', 'GET'])
+def q1():
     if request.method == 'POST':
-        return render_template(templates[1])
+        return redirect(url_for('q2'))
     else:
         return render_template(templates[0])
 
 
+@app.route("/q2", methods=['POST', 'GET'])
+def q2():
+    if request.method == 'POST':
+        return redirect(url_for('q3'))
+    else:
+        return render_template(templates[1])
+
+
+@app.route("/q3", methods=['POST', 'GET'])
+def q3():
+    if request.method == 'POST':
+        return redirect(url_for('q4'))
+    else:
+        return render_template(templates[2])
+
+
+@app.route("/q4", methods=['POST', 'GET'])
+def q4():
+    if request.method == 'POST':
+        return redirect(url_for('q5'))
+    else:
+        return render_template(templates[3])
+
+
+@app.route("/q5", methods=['POST', 'GET'])
+def q5():
+    if request.method == 'POST':
+        return redirect(url_for('wd'))
+    else:
+        return render_template(templates[4])
+
+
+@app.route("/wd", methods=['POST', 'GET'])
+def wd():
+    return render_template(templates[5])
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
